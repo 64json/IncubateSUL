cfd1d8fc9019a58e22ed980620b17563001250f6
Jason Park <parkjs814@gmail.com>
2016-05-16 13:22:55 -0500

    keep _README.md

--- template.sh
+++ template.sh
@@ -11,7 +11,7 @@ cd $city
 git remote set-url origin https://github.com/IncubateX/${city}.git
 git remote add upstream https://github.com/IncubateX/IncubateTemplate.git
 rm README.md
-mv _README.md README.md
+cp _README.md README.md
 sed -i "" "s/{city}/${city}/g" README.md
 sed -i "" "s/{CITY}/${CITY}/g" README.md
 git add .
